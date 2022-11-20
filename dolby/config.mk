80ec890648766b865150e1a86f2595c4db7b6ba7
saku-bruh <notwolfyy@gmail.com>
2022-11-20 19:42:55 +0200

    pdx215-extra: fix dolby
    Nuked properties in config.mk that made it break SafetyNet.
    Also added another blob that's required for dolby to work.

--- dolby/config.mk
+++ dolby/config.mk
@@ -2,16 +2,6 @@
 
 $(call inherit-product, vendor/sony/pdx215-extra/dolby/dolby.mk)
 
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.vendor.dolby.dax.version=DAX3_3.5.1.28_r1 \
-	vendor.audio.dolby.ds2.enabled=false \
-	vendor.audio.dolby.ds2.hardbypass=false \
-	ro.vendor.product.device.db=OP_DEVICE \
-	ro.vendor.product.manufacturer.db=OP_PHONE \
-	vendor.product.device=OP_PHONE \
-	vendor.product.manufacturer=OPD
-
-# Sepolicy
+# SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += \
     vendor/sony/pdx215-extra/dolby/sepolicy/vendor
